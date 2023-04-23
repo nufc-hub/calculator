a6452c98c41e92d336e8b31a11f56696f708a0a6
nufc-hub <kmchenouf@gmail.com>
2023-04-23 19:19:04 +0900

    Add variables and operate function

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-function add(array) {
-    return array.reduce((total, currentValue) => total + currentValue);
+function add(a, b) {
+    return a + b;
 }
 
-function subtract(array) {
-    return array.reduce((total, currentValue) => total - currentValue);
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply (array) {
-    return array.reduce((total, currentValue) => total * currentValue);
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide (array) {
-    return array.reduce((total, currentValue) => total / currentValue);
+function divide(array) {
+    return a / b;
 }
 
+let firstNum = 3;
+let secondNum = 7;
+let operator = '+';
+
+function operate() {
+    switch(operator) {
+        case '+':
+            return add(firstNum, secondNum);
+            break;
+        case '-':
+            return subtract(firstNum, secondNum);
+            break;
+        case '*':
+            return multiply(firstNum, secondNum);
+            break;
+        case '/':
+            return divide(firstNum, secondNum);
+            break;
+        default:
+            return 'E';
+            break;
+    }
+}
+
+console.log(operate());
