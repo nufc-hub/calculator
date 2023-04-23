10aaf3379c9f6214f683479426f037f592e1c689
nufc-hub <kmchenouf@gmail.com>
2023-04-23 14:19:06 +0900

    Add functions for basic maths operators

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(array) {
+    return array.reduce((total, currentValue) => total + currentValue);
+}
+
+function subtract(array) {
+    return array.reduce((total, currentValue) => total - currentValue);
+}
+
+function multiply (array) {
+    return array.reduce((total, currentValue) => total * currentValue);
+}
+
+function divide (array) {
+    return array.reduce((total, currentValue) => total / currentValue);
+}
+
