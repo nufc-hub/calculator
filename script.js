37cd8f9aeff14cef22c6ad86449067f153d5c8a4
nufc-hub <kmchenouf@gmail.com>
2023-05-01 22:39:35 +0900

    Add if else statement to divide function

--- script.js
+++ script.js
@@ -27,26 +27,26 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    if (b == 0) {
+        displayMain.textContent = 'LOL'
+    } else {
+        return a / b;
+    }
 }
 
 function operate() {
     switch(operator) {
-        case "+":
+        case '+':
             return add(firstNum, secondNum);
-            break;
-        case "-":
+        case '-':
             return subtract(firstNum, secondNum);
-            break;
-        case "*":
+        case '*':
             return multiply(firstNum, secondNum);
-            break;
-        case "/":
+        case '/':
             return divide(firstNum, secondNum);
-            break;
         default:
-            return "E";
-            break;
+            return 'E';
+
     }
 }
 
@@ -54,37 +54,42 @@ function handleNumberButtonClick(button) {
     displayMain.textContent += button.textContent;
         if (operator === '') {
             firstNum = Number(firstNum += button.textContent);
+            displayMain.textContent = firstNum;/* Replaces the 0 in the main display with the next clicked number, 
+                                                if 0 is clicked as the very first number at the start of a calculation 
+                                                or if 0 is clicked straight after clicking clear. */
         } else {
             secondNum = Number(secondNum += button.textContent);
-            displayMain.textContent = secondNum;// this replaces the result in the main display with the next digit(s) to be calculated
+            displayMain.textContent = secondNum;/* Replaces the result in the main display with the next digit(s) 
+                                                to be calculated. */
         }
         console.log(firstNum);
         console.log(secondNum);
 }
 
 function handleOperatorButtonClick(button) {
-    if (operator !== '') {
+    if(operator !== '') {
         const result = operate();
-        const roundedResult = result.toFixed(5); //rounds result to within 5 decimal places
-        const resultToNumber = Number(roundedResult);// changes roundedResult from string to number
+        const roundedResult = result.toFixed(5); // Rounds result to within 5 decimal places.
+        const resultToNumber = Number(roundedResult);// Changes roundedResult from string to number.
         firstNum = resultToNumber;
-        displayMain.textContent = firstNum; //this adds the result of the most recent calculation to the main display
-        displayMinor.textContent += secondNum; //this adds the secondNum variable to the minor display
-        secondNum = 0; //this then sets the secondNum variable back to 0 to clear it for the next calculation
+        displayMain.textContent = firstNum; // Adds the result of the most recent calculation to the main display.
+        displayMinor.textContent += secondNum; // Adds the secondNum variable to the minor display.
+        secondNum = 0; // This then sets the secondNum variable back to 0 to clear it for the next calculation.
         operator = button.textContent;
-        displayMinor.textContent += operator; //this adds the clicked operator value to the minor display
+        displayMinor.textContent += operator; // Adds the clicked operator value to the minor display.
     } else {
         operator = button.textContent;
-        displayMinor.textContent += firstNum; //this and...
-        displayMinor.textContent += operator; //this moves the first number and first operator in the calculation to the minor display as well ass all following numbers and operators, when true
+        displayMinor.textContent += firstNum; // This and...
+        displayMinor.textContent += operator; /* this moves the first number and first operator in the calculation
+                                            to the minor display as well ass all following numbers and operators, when true. */
     }            
     console.log(operator);
 }
 
 function handleOperateButtonClick() {
     const result = operate();
-    const roundedResult = result.toFixed(5); // rounds result to within 5 decimal places
-    const resultToNumber = Number(roundedResult); // changes roundedResult from string to number
+    const roundedResult = result.toFixed(5); // Rounds result to within 5 decimal places.
+    const resultToNumber = Number(roundedResult); // Changes roundedResult from string to number.
     firstNum = resultToNumber;
     displayMain.textContent = firstNum;
     secondNum = 0;
@@ -97,26 +102,26 @@ function handleClearButtonClick() {
     secondNum = 0;
     operator = '';
     displayMain.textContent = '';
-    displayMinor.textContent = ''
+    displayMinor.textContent = '';
 }
 
-numberButtons.forEach(function(button) {
+numberButtons.forEach(function(button) { // Gives functionality to the 'number' buttons.
     button.addEventListener('click', function() {
         handleNumberButtonClick(button);
     });
 });
 
-operatorButtons.forEach(function(button) {
+operatorButtons.forEach(function(button) { // Gives functionality to the 'operator' buttons.
     button.addEventListener('click', function() {
         handleOperatorButtonClick(button);
     });
 });
 
-operateButton.addEventListener('click', function() { // this gives functionality to the '=' button
+operateButton.addEventListener('click', function() { // Gives functionality to the '=' button.
     handleOperateButtonClick();
 });
 
-clearBtn.addEventListener('click', function () {
+clearBtn.addEventListener('click', function () { // Gives functionality to the 'clear' button.
     handleClearButtonClick();
 });
 
