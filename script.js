5ac24753b637d566db66fa192dd1bdb48b8530cc
nufc-hub <kmchenouf@gmail.com>
2023-05-11 22:05:29 +0900

    Add exponentiation function
    Improve divide by 0 functionality when updating the sub display.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const subDisplay = document.querySelector('.sub-display');
 
-const displayMain = document.querySelector('.display-main');
+const mainDisplay = document.querySelector('.main-display');
 
 const numberButtons = document.querySelectorAll('.number-btn');
 
 const decimalButton = document.querySelector('.point');
 
-const plusMinusButton = document.querySelector('.plus-minus')
+const plusMinusButton = document.querySelector('.plus-minus');
 
 const operatorButtons = document.querySelectorAll('.operator');
 
@@ -14,12 +14,16 @@ const operateButton = document.querySelector('.operate');
 
 const clearBtn = document.querySelector('.clear');
 
-const backspaceBtn = document.querySelector('.backspace')
+const backspaceBtn = document.querySelector('.backspace');
 
 window.onkeydown = function(e) {
     let key = e.key;
     let choice;
     switch (key) {
+        case 'n'://The letter n is used to denote 'negative'.
+            choice = document.querySelector('.plus-minus');
+            choice.click();
+            break;
         case '0':
             choice = document.querySelector('#zero');
             choice.click();
@@ -80,6 +84,10 @@ window.onkeydown = function(e) {
             choice = document.querySelector('#divide');
             choice.click();
             break;
+        case '^':
+            choice = document.querySelector('#exponentiation');
+            choice.click();
+            break
         case 'Enter':
         case '=':
             choice = document.querySelector('#equals')
@@ -117,6 +125,10 @@ function divide(a, b) {
     return a / b;
 }
 
+function power(base, exponent) {
+    return Math.pow(base, exponent);
+}
+
 function operate() {
     switch(operator) {
         case '+':
@@ -127,6 +139,8 @@ function operate() {
             return multiply(firstNum, secondNum);
         case '/':
             return divide(firstNum, secondNum);
+        case '^':
+            return power(firstNum, secondNum);
         default:
             return 'E';
 
@@ -134,31 +148,37 @@ function operate() {
 }
 
 function handleNumberButtonClick(button) {
-    const displayMainLength = 15
+    const mainDisplayLength = 15
     const maxDecimalLength = 10
 
-    if (displayMain.textContent.length === 15){
+    if (mainDisplay.textContent.length === 15) {
         return;
     }
+
+    if (mainDisplay.textContent === 'LOL') {
+        subDisplay.textContent += operator; /* Divide symbol is removed from sub display when divide by 0 is attempted.
+                                            This replaces the operator in the sub display with the operator the most current
+                                            calculation is being operated on. */
+    }
     
-    if (displayMain.textContent.includes('.')) {
-        const decimalPosition = displayMain.textContent.indexOf('.');
-        const maxLength = displayMainLength - decimalPosition + maxDecimalLength;
-            if (displayMain.textContent.length === maxLength) {
+    if (mainDisplay.textContent.includes('.')) {
+        const decimalPosition = mainDisplay.textContent.indexOf('.');
+        const maxLength = mainDisplayLength - decimalPosition + maxDecimalLength;
+            if (mainDisplay.textContent.length === maxLength) {
                 return;
             }
     } 
     
-    displayMain.textContent += button.textContent;
+    mainDisplay.textContent += button.textContent;
     
     if (operator === '') {
         firstNum = firstNum += button.textContent;
-        displayMain.textContent = firstNum;/* Replaces the 0 in the main display with the next clicked number, 
+        mainDisplay.textContent = firstNum;/* Replaces the 0 in the main display with the next clicked number, 
                                             if 0 is clicked as the very first number at the start of a calculation 
                                             or if 0 is clicked straight after clicking clear. */
     } else {
         secondNum = secondNum += button.textContent;
-        displayMain.textContent = secondNum;/* Replaces the result in the main display with the next digit(s) 
+        mainDisplay.textContent = secondNum;/* Replaces the result in the main display with the next digit(s) 
                                             to be calculated. */
     }
     console.log(firstNum);
@@ -167,43 +187,66 @@ function handleNumberButtonClick(button) {
 
 function handleDecimalButtonClick() {
     if (firstNum !== '' && operator !== '' && secondNum === '') {
-        displayMain.textContent = '0.'
+        mainDisplay.textContent = '0.'
         secondNum = '0.'
     } else if (firstNum === '' && operator === '' && secondNum === '' ) {
-        displayMain.textContent = '0.'
+        mainDisplay.textContent = '0.'
         firstNum = '0.'
     } else if (firstNum.includes('.') && secondNum === '') {
         return;
     } else if (firstNum !== '' && secondNum.includes('.')) {
         return;
     } else if (firstNum !== '' && operator !== '' && secondNum === '') {
-        displayMain.textContent = '.'
+        mainDisplay.textContent = '.'
     } else if (secondNum === '') {
         firstNum += decimalButton.textContent;
-        displayMain.textContent += decimalButton.textContent;
+        mainDisplay.textContent += decimalButton.textContent;
     } else {
         secondNum += decimalButton.textContent;
-        displayMain.textContent += decimalButton.textContent;
+        mainDisplay.textContent += decimalButton.textContent;
     }
 }
 
 function handlePlusMinusButtonClick() {
     if (firstNum.includes('-')) {
         firstNum = firstNum.slice(1);
-        displayMain.textContent = firstNum;
+        mainDisplay.textContent = firstNum;
     } else {
         const minus = '-';
         firstNum = minus + firstNum;
-        displayMain.textContent = firstNum
+        mainDisplay.textContent = firstNum
     }
 }
 
 function handleOperatorButtonClick(button) {
-    if (operator === '/' && secondNum === '0') {
-        displayMain.textContent = 'LOL'
+    if (mainDisplay.textContent.length === 15) { 
+        mainDisplay.textContent = '';
+                                                    /* This clears the main display when an operator button is clicked,
+                                                    if the display length is === to 15. This was added because 
+                                                    if the display length === 15 and the main display is not cleared,
+                                                    no more calculations can be done as the firstNum variable (which also
+                                                    acts as the result), stays in the main display after pressing the operator
+                                                    and therefore secondNum can't be entered. */
+    }
+    
+    if (mainDisplay.textContent === 'LOL') { //Prevents bugs due to replacing sub display operator values when dividing by 0.
+        mainDisplay.textContent = '';
         operator = button.textContent;
+        subDisplay.textContent += operator;
+        return;
+    } else if (operator === '/' && secondNum === '0') {
+        mainDisplay.textContent = 'LOL';
+        secondNum = ''; /* This is set to '' otherwise when typing a number after the LOL message, 
+                        a 0 will appear on the main display. This is because the 0 would still be left in the secondNum
+                        variable from the divide by 0 operation. */
+        modifySubDisplay = subDisplay.textContent.slice(0, -1);
+        subDisplay.textContent = modifySubDisplay;  /* This will remove the /(divide) icon from the sub display.
+                                                        This is necessary otherwise sub display will be left with
+                                                        a redundant divide symbol. This is redundant because
+                                                        the user's next operation may not be to divide. */
+
     } else if (firstNum === '' && operator === '' && secondNum === '') {
-        displayMain.textContent = '';
+        mainDisplay.textContent = '';
     } else if (firstNum !== '' && operator !== '' && secondNum === '') {
         operator = button.textContent;
         modifySubDisplay = subDisplay.textContent.slice(0, -1);
@@ -215,7 +258,7 @@ function handleOperatorButtonClick(button) {
         const roundedResult = result.toFixed(10); // Rounds result to within 5 decimal places.
         const resultToNumber = parseFloat(roundedResult);// Changes roundedResult from string to number.
         firstNum = String(resultToNumber);
-        displayMain.textContent = firstNum; // Adds the result of the most recent calculation to the main display.
+        mainDisplay.textContent = firstNum; // Adds the result of the most recent calculation to the main display.
         subDisplay.textContent += secondNum; // Adds the secondNum variable to the sub-display.
         secondNum = ''; // This then sets the secondNum variable back to 0 to clear it for the next calculation.
         operator = button.textContent;
@@ -231,7 +274,10 @@ function handleOperatorButtonClick(button) {
 
 function handleOperateButtonClick() {
     if (operator === '/' && secondNum === '0') {
-        displayMain.textContent = 'LOL'
+        mainDisplay.textContent = 'LOL';
+        modifySubDisplay = subDisplay.textContent.slice(0, -1);
+        subDisplay.textContent = modifySubDisplay;
+        secondNum = '';
     } else if (secondNum === '') {
         return; // When '=' button is pressed but secondNum is empty, nothing happens.
     } else {
@@ -241,7 +287,7 @@ function handleOperateButtonClick() {
         const roundedResult = result.toFixed(10); // Rounds result to within 5 decimal places.
         const resultToNumber = parseFloat(roundedResult); // Changes roundedResult from string to number.
         firstNum = String(resultToNumber);
-        displayMain.textContent = firstNum;
+        mainDisplay.textContent = firstNum;
         subDisplay.textContent = subDisplay.textContent + secondNum + '='; /* this adds the secondNum variable that has just been operated on to the subDisplay
                                                                                 along with an '=' sign. This is so the user can continue to see the history of their
                                                                                 calculation. */ 
@@ -255,25 +301,25 @@ function handleClearButtonClick() {
     firstNum = '';
     secondNum = '';
     operator = '';
-    displayMain.textContent = '';
+    mainDisplay.textContent = '';
     subDisplay.textContent = '';
 }
 
 function handleBackspaceButtonClick() {
 
     if (firstNum !== '' && operator !== '' && secondNum === '') {
-        displayMain.textContent = '';
+        mainDisplay.textContent = '';
     } else if (firstNum !== '' && operator === '' && secondNum === '' ) {
-        displayMain.textContent = displayMain.textContent.slice(0, -1);
+        mainDisplay.textContent = mainDisplay.textContent.slice(0, -1);
         firstNum = firstNum.toString().slice(0, -1);
     } else if (operator === '' && secondNum === '') {
-        displayMain.textContent = displayMain.textContent.slice(0, -1);
+        mainDisplay.textContent = mainDisplay.textContent.slice(0, -1);
         firstNum = firstNum.toString().slice(0, -1);
-        displayMain.textContent = firstNum;
+        mainDisplay.textContent = firstNum;
     } else {
-        displayMain.textContent = displayMain.textContent.slice(0, -1);
+        mainDisplay.textContent = mainDisplay.textContent.slice(0, -1);
         secondNum = secondNum.toString().slice(0, -1);
-        displayMain.textContent = secondNum;
+        mainDisplay.textContent = secondNum;
     }
 }
 
